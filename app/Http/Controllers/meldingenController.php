f4714815340256797618144fdbc5f37f5bb992ec
christiancoremans <christiancoremans1@hotmail.com>
2024-03-28 10:17:13 +0100

    Gedeeltelijk gefix
    nog verbeteren

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -11,7 +11,18 @@ echo $attractie . " / " . $capaciteit . " / " . $melder;
 require_once '../../../config/conn.php';
 
 //2. Query
+$type = "standaardwaarde"; // Je kunt hier een standaardwaarde toewijzen of deze uit het formulier halen, afhankelijk van jouw vereisten
+
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder, type) VALUES(:attractie, :capaciteit, :melder, :type)";
+
+
 
 //3. Prepare
+$statement = $conn->prepare($query);
 
 //4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":capaciteit" => $capaciteit,
+    ":melder" => $melder
+]);
